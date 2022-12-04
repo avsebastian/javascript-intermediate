46d5810c633549d789fdd1e7721103176e398d14
avsebastian <acostavannisebastian@gmail.com>
2022-12-04 14:12:26 -0300

    add exercise of basic data structure

---
+++ BasicDataStructures/exercise2.js
@@ -0,0 +1,8 @@
+/**
+ * In order to complete this challenge, set the 2nd position (index 1) 
+ * of myArray to anything you want, besides the letter b.
+ */
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "test";
+console.log(myArray);
---
+++ BasicDataStructures/exercise7.js
@@ -0,0 +1,12 @@
+/**
+ * We have defined a function, forecast, that takes an array as an argument. 
+ * Modify the function using slice() to extract information from the argument array and return a 
+ * new array that contains the string elements warm and sunny.
+ */
+
+ function forecast(arr) {
+    let array = arr.slice(2,4);
+    return array;
+  }
+  
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
